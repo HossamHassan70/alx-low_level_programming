e02467a5ce5ce878114efea72a6a158c964b9c4f
Hossam Hassan <hossam.sahss120@gmail.com>
2023-07-13 14:26:58 -0700

    Auto Commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - this is simple description for main function
+*return - always 0 (sucsess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+/**
+*return - always to 0 (sucsess)
+*/
+	return (0);
+}
