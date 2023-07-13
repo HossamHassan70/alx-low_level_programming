1a81135dab4ea6dbe0eefb9c2ae3c20b523a417f
Hossam Hassan <hossam.sahss120@gmail.com>
2023-07-14 02:05:14 +0300

    Auto Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
-#include <sys/syscall.h>
 /**
-  *main - Entry point
-  *Return: always 1 (error)
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ * Return: 1 (error).
 */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg));
-	return 1;
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
 }
