c2d569e19ac6dc13c5a5235cc9108ff4e5e762cc
Hossam Hassan <hossam.sahss120@gmail.com>
2023-08-10 13:29:55 -0700

    Auto Commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -78,7 +78,7 @@ int main(int argc, char const *argv[])
 		_puts("Error ");
 		exit(98);
 	}
-	print_int(_atoi(argv[1]) * _atoi(agrv[2]));
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
 	_putchar('\n');
 
 	return (0);
