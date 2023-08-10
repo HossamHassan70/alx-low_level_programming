d65da503a71d1bcaa4384c9842b3f75c3cbab73e
Hossam Hassan <hossam.sahss120@gmail.com>
2023-08-10 13:27:09 -0700

    Auto Commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+* _puts - A function to prints a string followed new line.
+* @str: Pointer to the sting to print.
+* Return: No return.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+* _atoi - A function that covert a string to an integer.
+* @s: A char type string.
+* Return: To integer converted.
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+			sign *= -1;
+	}
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+* print_int - A finction that prints an integer.
+* @n: int.
+* Return: No.
+ */
+
+void print_int(unsigned long int n)
+{
+	unsigned long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	/*To return integer to charachter*/
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+}
+
+/**
+* main - Entry point to print the result of muliplication.
+* @argc: int.
+* @argv: list.
+* Return: 0 (Sucsess)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(agrv[2]));
+	_putchar('\n');
+
+	return (0);
+}
