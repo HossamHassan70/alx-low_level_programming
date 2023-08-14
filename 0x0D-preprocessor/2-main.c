8631efb7c2e6bbeee2594fcc4fa405596e233894
Hossam Hassan <hossam.sahss120@gmail.com>
2023-08-14 13:05:51 -0700

    Auto Commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point that prints the name of the file
+* Description : Using __file__ function
+* Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
