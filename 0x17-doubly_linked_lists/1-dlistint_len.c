7bed3143e8174b15cb061dc50ad5a842b39216ed
Hossam Hassan <hossam.sahss120@gmail.com>
2024-04-03 05:28:23 +0200

    Modifiy some style in first 4 tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,14 +7,15 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *node = h;
-    size_t cont = 0;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-    while (node)
-    {
-        cont++;
-        node = node->next;
-    }
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
 
-    return (cont);
+	return (cont);
 }
+
