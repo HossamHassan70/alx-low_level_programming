840923500f06d0889c7faa54a36bcd1fdc931c08
Hossam Hassan <99551852+HossamHassan70@users.noreply.github.com>
2024-07-19 00:48:37 +0300

    All Tasks for 0x1E-search_algorithms project

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <lists.h>
+
+/* this script for Prototype*/
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+#endif
