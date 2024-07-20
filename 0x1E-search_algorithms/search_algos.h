413d4d02bf3de24fd77bb349d0c71c2c8d330681
Hossam Hassan <hossam.sahss120@gmail.com>
2024-07-20 18:04:34 +0300

    modify 100-jump and header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,11 @@
 #define SEARCH_H
 
 #include <stdio.h>
+#include <math.h>
 
 /* this script for Prototype*/
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
 #endif
