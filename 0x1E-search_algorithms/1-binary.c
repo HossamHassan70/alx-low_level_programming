bd3044383f05d161d67a4252b21394ac031f3504
Hossam Hassan <hossam.sahss120@gmail.com>
2024-07-20 18:08:53 +0300

    fix a bug in 1-binary.c file

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,51 @@
 #include "search_algos.h"
 
-void print_array(int *array, size_t i, size_t size);
-
 /**
- * binary_search - this method binary search for algorithm in a str
- * @array: for list of elements
- * @size: for size of the list
- * @value: for Target to search
- * Return: the element Index on success (-1) on fail
+ * print_arr - Helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
  */
-int binary_search(int *array, size_t size, int value)
+void print_arr(int *array, size_t l, size_t r)
 {
-	size_t low = 0, high = size - 1, half = 0;
+	size_t i = 0;
 
-	if (array == NULL || value == 0)
-		return (-1);
-
-	print_array(array, low, size);
-	while (low < high)
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
 	{
-		half = (low + high) / 2;
-		if (array[half] < value)
-		{
-			low = half + 1;
-			print_array(array, low, high + 1);
-		}
-		else if (array[half] > value)
-		{
-			high = half - 1;
-			print_array(array, low, high + 1);
-		}
+		if (i != r)
+			printf("%d, ", array[i]);
 		else
-			return (array[half]);
+			printf("%d\n", array[i]);
 	}
-	return (-1);
 }
 
 /**
- * print_array - this method Print for  all elements in an array
- * @array: for list of elements
- * @i: for Index to init
- * @size: for size of the array
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
  */
-void print_array(int *array, size_t i, size_t size)
+int binary_search(int *array, size_t size, int value)
 {
-	printf("Searching in array: ");
-	while (i < size)
+	size_t mid = 0;
+	size_t l = 0;
+	size_t r = size - 1;
+
+	if (!array)
+		return (-1);
+
+	while (l <= r)
 	{
-		if (i + 1 == size)
-			printf("%d ", array[i]);
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
 		else
-			printf("%d, ", array[i]);
-		i++;
-	} printf("\n");
+			l = mid + 1;
+	}
+	return (-1);
 }
