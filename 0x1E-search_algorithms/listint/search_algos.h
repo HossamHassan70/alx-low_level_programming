e49173898ac60c8ca40a221e1005b6a0fd5beba1
Hossam Hassan <hossam.sahss120@gmail.com>
2024-07-21 05:30:29 +0300

    remaining tasks

--- 0x1E-search_algorithms/listint/search_algos.h
+++ 0x1E-search_algorithms/listint/search_algos.h
@@ -1,3 +1,5 @@
+#ifndef DD304B0E_A603_4EEB_92D3_23C890D38461
+#define DD304B0E_A603_4EEB_92D3_23C890D38461
 #ifndef SEARCH_H
 #define SEARCH_H
 
@@ -12,25 +14,31 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+
+
 
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
+ * struct listint_s - singly linked list
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express lane
+ * Description: singly linked list node structure
  */
-typedef struct skiplist_s
+typedef struct listint_s
 {
 int n;
 size_t index;
-struct skiplist_s *next;
-struct skiplist_s *express;
-} skiplist_t;
+struct listint_s *next;
+} listint_t;
+
 
 #endif
+
+
+#endif /* DD304B0E_A603_4EEB_92D3_23C890D38461 */
